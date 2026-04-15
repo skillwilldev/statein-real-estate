fbe02ce5cc5ef6584c173e9270d6d72fc9a3e068
Melik <melikpapikyan1@gmail.com>
2026-04-15 12:37:13 +0400

    estate markup gsap animation

--- estate/script.js
+++ estate/script.js
@@ -1,4 +1,5 @@
 gsap.registerPlugin(ScrollTrigger);
+let mm = gsap.matchMedia();
 
 gsap.from('.dream__items li', {
     opacity: 0, y: 28, duration: 0.6,
@@ -13,7 +14,7 @@ gsap.from('.dream__items li', {
 
 
 gsap.from('header', {
-    opacity: 0, y: -28, duration: 0.6,
+    opacity: 0, y: -28, duration: 0.7,
     ease: 'power2.out',
     scrollTrigger: {
         trigger: 'header',
@@ -29,28 +30,59 @@ gsap.from('header', {
 
 
 
+mm.add("(min-width: 768px)", () => {
+    gsap.from('.team__card', {
+        opacity: 0, y: 28, duration: 0.6,
+        stagger: 0.3, ease: 'power2.out',
+        scrollTrigger: {
+            trigger: '.team__card',
+            start: 'top 85%',
+            once: true,
 
-gsap.from('.team__card', {
-    opacity: 0, y: 28, duration: 0.6,
-    stagger: 0.2, ease: 'power2.out',
-    scrollTrigger: {
-        trigger: '.team__card',
-        start: 'top 85%',
-        once: true,
-
-    }
+        }
+    });
 });
 
-gsap.from('.achievements__top.anime1', {
-    opacity: 0, x: -100, duration: 0.6,
-    stagger: 0.2, ease: 'power2.out',
-    scrollTrigger: {
-        trigger: '.achievements__top.anime1',
-        start: 'top 85%',
-        once: true,
+mm.add("(max-width: 768px)", () => {
+    gsap.from('.team__card', {
+        opacity: 0, y: 28, duration: 0.6,
+        stagger: 1, ease: 'power2.out',
+        scrollTrigger: {
+            trigger: '.team__card',
+            start: 'top 85%',
+            once: true,
 
-    }
+        }
+    });
 });
+
+mm.add("(min-width: 768px)", () => {
+    gsap.from('.achievements__top.anime1', {
+        opacity: 0, x: -100, duration: 0.6,
+        stagger: 0.2, ease: 'power2.out',
+        scrollTrigger: {
+            trigger: '.achievements__top.anime1',
+            start: 'top 85%',
+            once: true,
+
+        }
+    });
+});
+
+mm.add("(max-width: 768px)", () => {
+    gsap.from('.achievements__top.anime1', {
+        opacity: 0, x: -100, duration: 0.6,
+        stagger: 0.6, ease: 'power2.out',
+        scrollTrigger: {
+            trigger: '.achievements__top.anime1',
+            start: 'top 85%',
+            once: true,
+
+        }
+    });
+});
+
+
 
 gsap.from('.achievements__top.exp-top', {
     opacity: 0, x: -100, duration: 0.6,
@@ -87,24 +119,53 @@ gsap.from('.achievements__top.enterprises-top', {
 
 
 
-gsap.from('.achievements__item', {
-    opacity: 0, y: 28, duration: 0.6,
-    stagger: 0.3, ease: 'power2.out',
-    scrollTrigger: {
-        trigger: '.achievements__item',
-        start: 'top 85%',
-        once: true,
-    }
+mm.add("(min-width: 768px)", () => {
+    gsap.from('.achievements__item', {
+        opacity: 0, y: 28, duration: 0.6,
+        stagger: 0.3, ease: 'power2.out',
+        scrollTrigger: {
+            trigger: '.achievements__item',
+            start: 'top 85%',
+            once: true,
+        }
+    });
 });
 
-gsap.from('.experience__item', {
-    opacity: 0, y: 28, duration: 0.6,
-    stagger: 0.2, ease: 'power2.out',
-    scrollTrigger: {
-        trigger: '.experience__item',
-        start: 'top 85%',
-        once: true,
-    }
+mm.add("(max-width: 768px)", () => {
+    gsap.from('.achievements__item', {
+        opacity: 0, y: 28, duration: 0.6,
+        stagger: 0.6, ease: 'power2.out',
+        scrollTrigger: {
+            trigger: '.achievements__item',
+            start: 'top 85%',
+            once: true,
+        }
+    });
+});
+
+
+mm.add("(min-width: 768px)", () => {
+    gsap.from('.experience__item', {
+        opacity: 0, y: 28, duration: 0.6,
+        stagger: 0.3, ease: 'power2.out',
+        scrollTrigger: {
+            trigger: '.experience__item',
+            start: 'top 85%',
+            once: true,
+        }
+    });
+});
+
+mm.add("(max-width: 768px)", () => {
+    gsap.from('.experience__item', {
+        opacity: 0, y: 28, duration: 0.6,
+        stagger: 0.6, ease: 'power2.out',
+        scrollTrigger: {
+            trigger: '.experience__item',
+            start: 'top 85%',
+            once: true,
+        }
+    });
 });
 
 
@@ -121,14 +182,28 @@ gsap.from('.story__values', {
 
 
 
-gsap.from('.title-block', {
-    opacity: 0, y: 28, duration: 0.6,
-    stagger: 0.2, ease: 'power2.out',
-    scrollTrigger: {
-        trigger: '.title-block',
-        start: 'top 85%',
-        once: true,
-    }
+mm.add("(min-width: 768px)", () => {
+    gsap.from('.title-block', {
+        opacity: 0, y: 28, duration: 0.6,
+        stagger: 0.3, ease: 'power2.out',
+        scrollTrigger: {
+            trigger: '.title-block',
+            start: 'top 85%',
+            once: true,
+        }
+    });
+});
+
+mm.add("(max-width: 768px)", () => {
+    gsap.from('.title-block', {
+        opacity: 0, y: 28, duration: 0.6,
+        stagger: 0.6, ease: 'power2.out',
+        scrollTrigger: {
+            trigger: '.title-block',
+            start: 'top 85%',
+            once: true,
+        }
+    });
 });
 
 gsap.from('.dream', {
@@ -142,14 +217,28 @@ gsap.from('.dream', {
 });
 
 
-gsap.from('.story__trust', {
-    opacity: 0, y: 28, duration: 0.6,
-    stagger: 0.2, ease: 'power2.out',
-    scrollTrigger: {
-        trigger: '.story__trust',
-        start: 'top 85%',
-        once: true,
-    }
+mm.add("(min-width: 768px)", () => {
+    gsap.from('.story__trust', {
+        opacity: 0, y: 28, duration: 0.6,
+        stagger: 0.3, ease: 'power2.out',
+        scrollTrigger: {
+            trigger: '.story__trust',
+            start: 'top 85%',
+            once: true,
+        }
+    });
+});
+
+mm.add("(max-width: 768px)", () => {
+    gsap.from('.story__trust', {
+        opacity: 0, y: 28, duration: 0.6,
+        stagger: 0.6, ease: 'power2.out',
+        scrollTrigger: {
+            trigger: '.story__trust',
+            start: 'top 85%',
+            once: true,
+        }
+    });
 });
 
 
